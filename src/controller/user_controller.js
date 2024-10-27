8d251747228335853f9ba92b61ba68f2cd1911b6
2002bish <bniroula707@gmail.com>
2024-10-27 07:50:26 +0545

    user controller added

--- src/controller/user_controller.js
+++ src/controller/user_controller.js
@@ -2,6 +2,22 @@ import { User } from "../model/user_model";
 
 const editProfile= async (req, res) => {
     try {
-        const user= await User.findByIdAndUpdate(req.user._id,req.body,{})
+        const user= await User.findByIdAndUpdate(req.user._id,req.body,{new: true})
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({message: "Error occur in profile "});
     }
+};
+const orderViewHiostory= async (req, res) => {
+    try{
+        const order= await User.findById(req.user._id).populate("orders");
+        res.status(200).json(user.orders);
+    }
+    catch (error) {
+        req.status(500).json({message: "Error factching order history"});
+    }
+}
+export {
+editProfile,
+orderViewHiostory
 }
